b82ccf12c1559de03d63f25ac4aa799dce166e9d
Wagon706 <66565178+Wagon706@users.noreply.github.com>
2025-12-31 08:10:53 +0200

    Delete Projects/littlelemon directory

--- Projects/littlelemon/littlelemon/littlelemon/__init__.py
+++
@@ -1,7 +0,0 @@
-try:
-	import pymysql
-	pymysql.install_as_MySQLdb()
-except Exception:
-	# Safe no-op if PyMySQL isn't installed yet
-	pass
-
--- Projects/littlelemon/littlelemon/tests/test_models.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from .models import MenuItem
-
-
-class MenuItemTest(TestCase):
-    def test_get_item(self):
-        item = MenuItem.objects.create(title="IceCream", price=80, inventory=100)
-        self.assertEqual(str(item), "IceCream : 80")
